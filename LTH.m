5b0a7a09943943a98197baa311a26380f0d4f807
jyjh <jerryyenjh@gmail.com>
2026-01-22 17:42:38 +0800

    Amended code to return the values instead
    Moved the printing portion of the function to be external, and changed the function to return the values instead.
    
    This is to make the function code a bit clearer, and also make it more portable in case we are asked to extend it later on. The core calculations of the function were left untouched.
    
    Also added some additional comments for clarity.

--- LTH.m
+++ LTH.m
@@ -1,13 +1,17 @@
-param.mass      = 265;   
-param.h_CG      = 0.300;  
-param.wheelbase = 1.600; 
-param.track_f = 1.20; 
-param.h_RC_f  = 0.06;  
-param.track_r = 1.18;  
-param.h_RC_r  = 0.08;  
-param.LLTD_f = 0.55;
+param.mass      = 265;
+param.h_CG      = 0.300; % height of center of gravity
+param.wheelbase = 1.600;
+param.track_f = 1.20; % front track
+param.h_RC_f  = 0.06;  % height of front roll center
+param.track_r = 1.18;  % rear track
+param.h_RC_r  = 0.08;  % height of rear roll center
+param.LLTD_f = 0.55; % lateral Load Transfer Distribution front
 
-function loadTransfer(param,g_mag,acc_dir)
+
+% Load transfer calculation function
+% g_mag: magnitude of acceleration in g's.
+% acc_dir: acceleration direction in degrees. (0 is front, 90 is left)
+function [latf_elastic, latf_geo, latr_elastic, latr_geo, long] = loadTransfer(param, g_mag, acc_dir)
 
 g = 9.81;
 
@@ -31,11 +35,18 @@ hR_avg = (hRf + hRr) / 2;
 latf_elastic = -m * ay * (hC - hR_avg) * LLTD_f/ tf;
 latr_elastic = -m * ay* (hC - hR_avg) *(1-LLTD_f)/ tr;
 
+long = -m * ax * hC / L;
+
+end
+
+g_mag   = input("g magnitude:");
+acc_dir = input("acceleration direction (0-360, 0 is front, 90 is left):");
+
+[latf_elastic, latf_geo, latr_elastic, latr_geo, long] = loadTransfer(param,g_mag,acc_dir);
+
 latf_total = latf_geo + latf_elastic;
 latr_total = latr_geo + latr_elastic;
 lat_total = latf_total + latr_total;
-
-long = -m * ax * hC / L;
 
 disp("lateral front geometric (N):" + latf_geo)
 disp("lateral front elastic (N):" + latf_elastic)
@@ -43,15 +54,8 @@ disp("lateral rear geometric (N):" + latr_geo)
 disp("lateral rear elastic (N):" + latr_elastic)
 disp("lateral front (N):" + latf_total)
 disp("lateral rear (N):" + latr_total)
-disp("lateral total (N):"+ lat_total)
+disp("lateral total (N):" + lat_total)
 disp("longitudinal (N):" + long)
 disp("negative means load transfer to right or to the back.")
-end
-
-g_mag   = input("g magnitude:");    
-acc_dir = input("acceleration direction (0-360, 0 is front, 90 is left):");    
-
-loadTransfer(param,g_mag,acc_dir)
-
 
 
